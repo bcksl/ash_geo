19bfef4b0b45b9728255cdd9857db752c1ab341e
bcksl <bcksl@proton.me>
2023-06-15 03:05:49 +0300

    fix: remove reduntant `docs` task from `mix check`

--- .check.exs
+++ .check.exs
@@ -3,7 +3,6 @@
     format: "mix format",
     outdated: "mix hex.outdated",
     deps_audit: "mix deps.audit",
-    docs: "mix docs",
     sobelow: "mix sobelow --config",
     migrations: "mix ash_postgres.generate_migrations --check",
     coveralls: "mix coveralls"
